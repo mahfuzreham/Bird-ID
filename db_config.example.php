6ed1a78d3adba908dde5ca637e6c1b871760a874
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2026-08-08 17:39:31 +0600

    Add database configuration template

---
+++ db_config.example.php
@@ -0,0 +1,6 @@
+<?php
+$DB_HOST='127.0.0.1';
+$DB_NAME='bird_id';
+$DB_USER='bird_user';
+$DB_PASS='CHANGE_ME';
+$pdo=new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4",$DB_USER,$DB_PASS,[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC]);
